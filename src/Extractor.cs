694ee5323878bf899dab7bf0e6d910faa8ac60e4
Nobuyuki Iwanaga <ufcpp@live.jp>
2020-02-17 18:30:15 +0900

    added Extractor

---
+++ src/Extractor.cs
@@ -0,0 +1,44 @@
+﻿using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using System.IO;
+
+namespace ExtractUnityPackage
+{
+    internal static class Extractor
+    {
+        public static void ImportUnityPackage(string packagePath, string destinationFolderPath)
+        {
+            var tempFolder = Path.Combine(Path.GetTempPath(), Path.GetFileName(packagePath) + ".unitypackage.extract");
+
+            using (var i = new FileStream(packagePath, FileMode.Open, FileAccess.Read))
+            using (var gi = new GZipInputStream(i))
+            using (var tar = TarArchive.CreateInputTarArchive(gi))
+            {
+                tar.ExtractContents(tempFolder);
+            }
+
+            foreach (var item in Directory.GetDirectories(tempFolder))
+            {
+                var pathname = File.ReadAllText(Path.Combine(item, "pathname"));
+                var path = Path.Combine(destinationFolderPath, pathname);
+
+                var assetPath = Path.Combine(item, "asset");
+                if (File.Exists(assetPath))
+                {
+                    var folder = Path.GetDirectoryName(Path.GetFullPath(path));
+                    if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+                    File.Copy(assetPath, path);
+                }
+                else
+                {
+                    var folder = path;
+                    if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+                }
+                var assetMetaPath = Path.Combine(item, "asset.meta");
+                File.Copy(assetMetaPath, path + ".meta");
+            }
+
+            Directory.Delete(tempFolder, true);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,27 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExtractUnityPackage
+{
+    [Command(Description = "A global tool for extracting .unitypackage", Name = "dotnet ExtractUnityPackage")]
+    public class Program
+    {
+        [Argument(0, Description = "source .unitypackage path")]
+        [Required]
+        public string Package { get; }
+
+        [Argument(1, Description = "destination folder path")]
+        [Required]
+        public string Destination { get; }
+
+        static void Main(string[] args)
+        {
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        public void OnExecute()
+        {
+            Extractor.ImportUnityPackage(Package, Destination);
+        }
+    }
+}
